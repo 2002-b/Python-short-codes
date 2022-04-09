fd0422cc85faca87aab5d50279099eda5c19e575
2002-b <63595748+2002-b@users.noreply.github.com>
2022-04-09 22:05:43 +0530

    Create heart.py

---
+++ heart.py
@@ -0,0 +1,13 @@
+# Python-short-codes
+Lets create some fun outof Python
+from turtle import*
+color("red")
+begin_fill()
+pensize(3)
+left(50)
+forward(144)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(144)
+end_fill()
